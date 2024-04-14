cb004ffaba4055dba4df7e4519271899de777ca7
Rustem Abdrashitov <rust.mos@gmail.com>
2024-04-14 16:27:57 +0400

    Implement main Vue.js application and App.vue
    The commit introduces the main JavaScript file (main.js) that bootstraps Vuetify and other plugins while also mounting the application. Besides, a new Vue component file (App.vue) was included, featuring Vuetify UI components. The README was also updated with relevant information.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# vuetify-rrule
+# Vuetify Rrule
+
+## Vue component for managing recurrence rule
+
+### Component dependencies:
+
+- [rrule.js](https://github.com/jkbrzt/rrule)
+- [vuetify.js](https://vuetifyjs.com/)
+- [moment.js](https://momentjs.com/guides/)
+- [vue-i18n](https://www.npmjs.com/package/vue-i18n)
+
+
+## 📑 License
+[MIT](http://opensource.org/licenses/MIT)
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-sheet width="800">
+        <v-card>
+          <v-toolbar>Example</v-toolbar>
+          <v-card-item>
+            <rrule-component/>
+          </v-card-item>
+        </v-card>
+      </v-sheet>
+
+
+
+    </v-main>
+
+    <app-footer />
+  </v-app>
+</template>
+
+<script setup>
+import RruleComponent from "@/components/rrule/RruleComponent.vue";
+import AppFooter from "@/components/AppFooter.vue";
+</script>
